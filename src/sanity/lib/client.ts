c1c7f7eaa5c451668e6d8585bc9e9e0f4657e921
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-10-19 11:55:41 +0500

    sanity added

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
