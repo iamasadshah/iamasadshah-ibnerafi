7f8366bad9f3884459387e6a340307a25b8d6c2d
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-11-16 11:18:51 +0500

    Links added

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,80 +1,81 @@
 import React from "react";
 import { FaGithub, FaHackerrank, FaMedium, FaLinkedin } from "react-icons/fa";
 import { FaUpwork } from "react-icons/fa6";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="flex flex-col lg:flex-row items-center justify-between bg-primary2 text-white py-6 -mt-1 px-10">
       {/* Pages Links */}
       <div className="flex space-x-6 mb-4 lg:mb-0 lg:space-x-10">
-        <a
+        <Link
           href="#about"
           className="text-sm lg:text-lg hover:text-primary transition duration-300"
         >
           About
-        </a>
-        <a
+        </Link>
+        <Link
           href="#services"
           className="text-sm lg:text-lg hover:text-primary transition duration-300"
         >
           Services
-        </a>
-        <a
+        </Link>
+        <Link
           href="#portfolio"
           className="text-sm lg:text-lg hover:text-primary transition duration-300"
         >
           Portfolio
-        </a>
-        <a
+        </Link>
+        <Link
           href="#contact"
           className="text-sm lg:text-lg hover:text-primary transition duration-300"
         >
           Contact
-        </a>
+        </Link>
       </div>
 
       {/* Social Icons */}
       <div className="flex space-x-4 lg:space-x-6">
-        <a
-          href="https://github.com"
+        <Link
+          href="https://github.com/iamasadshah"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 rounded-full bg-gray-800 hover:bg-primary transition duration-500 transform hover:scale-110"
         >
           <FaGithub size={24} />
-        </a>
-        <a
-          href="https://hackerrank.com"
+        </Link>
+        <Link
+          href="https://www.hackerrank.com/profile/asadibnerafi"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 rounded-full bg-gray-800 hover:bg-primary transition duration-500 transform hover:scale-110"
         >
           <FaHackerrank size={24} />
-        </a>
-        <a
-          href="https://upwork.com"
+        </Link>
+        <Link
+          href="https://www.upwork.com/freelancers/~0151539c3fe76e8c72?mp_source=share"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 rounded-full bg-gray-800 hover:bg-primary transition duration-500 transform hover:scale-110"
         >
           <FaUpwork size={24} />
-        </a>
-        <a
-          href="https://medium.com"
+        </Link>
+        <Link
+          href="https://medium.com/@ibnerafi"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 rounded-full bg-gray-800 hover:bg-primary transition duration-500 transform hover:scale-110"
         >
           <FaMedium size={24} />
-        </a>
-        <a
-          href="https://linkedin.com"
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/iamasadshah/"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 rounded-full bg-gray-800 hover:bg-primary transition duration-500 transform hover:scale-110"
         >
           <FaLinkedin size={24} />
-        </a>
+        </Link>
       </div>
     </footer>
   );
